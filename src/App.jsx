f874c707c552539951a02542f4084cd28c0e83da
aaa <aaa@aaa.aaa>
2026-02-04 13:53:49 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import routers from '@/routers/routers'
 import { Suspense } from 'react';
-import { SideBarProvider } from '@/contexts/SideBarProvider';
+import { SideBarProvider } from '@/contexts/SidebarProvider';
 import SideBar from '@components/SideBar/SideBar';
 import { ToastProvider } from '@/contexts/ToastProvider';
 
